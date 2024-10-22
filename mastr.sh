67d1918c6640419ffb1640d21c64bd488acc13ca
agora-edm-tech <edm-tech@agora-energiewende.de>
2024-10-22 14:37:30 +0200

    Adjusted version

--- mastr.sh
+++ mastr.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/jupyter-edm-jupyter/repos/open-MaStR-agora/
 chmod +x /home/jupyter-edm-jupyter/repos/open-MaStR-agora/main.py
-/home/jupyter-edm-jupyter/mambaforge/envs/py38_open_mastr/bin/python main.py
+/home/jupyter-edm-jupyter/mambaforge/envs/py310_open_mastr/bin/python main.py
